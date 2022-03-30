98f191c56df4ea8782053e4068e409ce758ba139
BairIT <beratema@yandex.ru>
2022-03-30 21:49:42 +0800

    ci.yaml с проверка

--- project/settings.py
+++ project/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = '1234'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['127.0.0.1', '8000']
+ALLOWED_HOSTS = []
 
 
 # Application definition
